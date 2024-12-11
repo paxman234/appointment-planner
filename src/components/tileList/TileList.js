84aee6c1d1bd4b9472cf7411cdb8188c09d94c1b
paxman234 <sav-fernandes@outlook.com>
2024-12-11 20:12:26 +0000

    step four add list of tile components rendered from tileList.

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Tile from '../tile/Tile'
+export const TileList = (props) => {
+  props.tileList.map((tile, index) => {
+    return (
+      <div>
+        <ul id="tileArray">
+          <li key={index}>
+            <Tile name={tile.name} description={tile.description}/>
+          </li>
+        </ul>
+      </div>
+    );
+  })
 
-export const TileList = () => {
-  return (
-    <div>
-      
-    </div>
-  );
 };
