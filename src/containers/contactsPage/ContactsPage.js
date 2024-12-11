2788c93345c240906116f194c40347a0a58f78b3
paxman234 <sav-fernandes@outlook.com>
2024-12-11 19:42:22 +0000

    step two handle and display contacts info on contacts page

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,14 +3,27 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName ] = useState("");
+  const [number, setNumber ] = useState("#");
+  const [email, setEmail] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    props.contacts.array.forEach(contact => {
+      if(contact.name === name) {
+        console.log("duplicate name entered")
+      } else {
+        console.log("no duplicate");
+        setName(name);
+      }
+      return;
+    });
+    document.getElementById("contactForm").reset();
     /*
     Add contact info and clear data
     if the contact name is not a duplicate
@@ -25,11 +38,24 @@ export const ContactsPage = () => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <form id="contactForm">
+          <div>
+            <input id="nameForm" placeholder="name" value={props.contacts[-1].name}></input>
+          </div>
+          <div>
+            <input id="telephoneNumberForm" placeholder="telephone number" value={props.contacts[-1].number} ></input>
+          </div>          
+          <div>
+            <input id="emailForm" placeholder="email" value={props.contacts[-1].email}></input>
+          </div>
+          <button type="button" onClick={handleSubmit}>Submit</button>
+        </form>
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={props.contacts} />
       </section>
     </div>
   );
