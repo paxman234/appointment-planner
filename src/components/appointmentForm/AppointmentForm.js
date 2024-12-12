e95bb4fc61b6290bad5e9337a817741633b97082
paxman234 <sav-fernandes@outlook.com>
2024-12-12 11:39:15 +0000

    pass props from appointment form to contact picker and implement select box options for contacts in contact picker.

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -63,7 +63,7 @@ export const AppointmentForm = ({
         </input>
       </div>
       <div>
-        <ContactPicker contactsList={props.contact} />
+        <ContactPicker name="contactPicker" value={props.contact[0]} onChange={handleContact} contactsList={props.contact} />
       </div>
       <button type="submit">Submit</button>
     </form>
--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = (props) => {
   return (
-    <></>
+    <select name={props.name} value={props.value} onChange={props.handleContact}>
+      <option value="">No Contact Selected</option>
+      {props.contact.map((cont) => {
+        <option>{cont.name}</option>
+      })}
+    </select>
   );
 };
