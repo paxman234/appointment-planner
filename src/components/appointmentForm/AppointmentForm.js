68154b045b0f346c7920a5f2e259745e17d6429a
paxman234 <sav-fernandes@outlook.com>
2024-12-16 19:17:34 +0000

    select changing to the selected option.

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -57,7 +57,7 @@ export const AppointmentForm = ({
         </input>
       </div>
       <div>
-        <ContactPicker name={contact?.name} value={contact? contact : ""} onChange={(e) => setContact(e.target.value)} contacts={contacts} required />
+        <ContactPicker name={contact?.name} value={contact} onChange={(key) => setContact(contacts[key])} contacts={contacts} required />
       </div>
       <button id="appointmentFormBtn" type="submit">Submit</button>
     </form>
--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -3,11 +3,18 @@ import React from "react";
 export const ContactPicker = ({name, value, contacts, onChange}) => {
   // const [selectedContact, setSelectedContact] = useState([]);
   // const contactOptions = contacts.map((contact) => contact["name"]);
+  const handleSelectOption = (e) => {
+    onChange(parseInt(e.target.value));
+  }
   return (
-    <select id="contactsList" form="appointmentForm" name={name} value onChange={onChange}>
+    <select id="contactsList" 
+    form="appointmentForm" 
+    name={name} 
+    selected={value ? value : ""} 
+    onChange={handleSelectOption} >
       <option name="default" value="">No Content Selected.</option>
       {contacts.map((contact, index) => (
-        <option key={index} name={contact["name"]} value={contact}>{contact.name}</option>
+        <option key={index} name={contact["name"]} value={index}>{contact.name}</option>
       )) 
       // : <options>Please add more contacts...</options>
       }
